fe219a6161f53725bfdca31a9c57fd455736526c
George Bentes <george.bentes@gmail.com>
2025-05-18 16:59:57 -0300

    feat: add handlers placeholders

--- main.go
+++ main.go
@@ -5,8 +5,19 @@ import (
 	"net/http"
 )
 
+func showPageListContacts(w http.ResponseWriter, r *http.Request) {
+	log.Println("show page to list contacts")
+}
+
+func showPageFormContacts(w http.ResponseWriter, r *http.Request) {
+	log.Println("Show page to add or edit contact")
+}
+
 func main() {
 	mux := http.NewServeMux()
+
+	mux.HandleFunc("GET /contacts", showPageListContacts)
+	mux.HandleFunc("GET /fcontacts", showPageFormContacts)
 
 	log.Println("Starting server on 4000")
 	err := http.ListenAndServe(":4000", mux)
