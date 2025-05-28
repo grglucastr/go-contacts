183755bc4479e5ffbb96de0695277aa7abd77153
grglucastr <georgebnunes@gmail.com>
2025-05-28 20:50:56 -0300

    feat: open db connection and add contacts models

--- cmd/web/v2/main.go
+++ cmd/web/v2/main.go
@@ -1,22 +1,48 @@
 package main
 
 import (
+	"database/sql"
 	"fmt"
 	"log"
 	"net/http"
+	"os"
+
+	_ "github.com/go-sql-driver/mysql"
+	v2 "github.com/grglucastr/go-contacts/internal/models/v2"
 )
 
 type application struct {
+	ContactModel *v2.ContactModel
 }
 
 func main() {
 
-	app := &application{}
+	db, err := openDB()
+	if err != nil {
+		log.Fatal(err.Error())
+		os.Exit(1)
+	}
+
+	defer db.Close()
+
+	app := &application{
+		ContactModel: &v2.ContactModel{DB: db},
+	}
 
 	fmt.Println("Loading server on port :4000")
-	err := http.ListenAndServe(":4000", app.routes())
+	err = http.ListenAndServe(":4000", app.routes())
 
 	if err != nil {
-		log.Panic(err.Error())
+		log.Fatal(err.Error())
+		os.Exit(1)
 	}
+}
+
+func openDB() (*sql.DB, error) {
+	conn, err := sql.Open("mysql", "gowebv2:webv2@/go_contacts_v2")
+	if err != nil {
+		return nil, err
+	}
+
+	return conn, nil
 }
---
+++ internal/models/v2/contacts.go
@@ -0,0 +1,72 @@
+package v2
+
+import "database/sql"
+
+type Contact struct {
+	ID             int32  `form:"id"`
+	Name           string `form:"name"`
+	RelationshipID int32  `form:"relationship_id"`
+}
+
+type ContactModel struct {
+	DB *sql.DB
+}
+
+func (m *ContactModel) Insert(name string, relationshipID int32) (int64, error) {
+
+	stmt := "INSERT INTO contacts (name, relationship_id) VALUE (?, ?)"
+
+	result, err := m.DB.Exec(stmt, name, relationshipID)
+	if err != nil {
+		return 0, err
+	}
+
+	id, err := result.LastInsertId()
+	if err != nil {
+		return 0, err
+	}
+
+	return id, nil
+}
+
+func (m *ContactModel) ListAllContacts() ([]Contact, error) {
+
+	stmt := "SELECT id, name, relationship_id FROM contacts"
+	rows, err := m.DB.Query(stmt)
+	if err != nil {
+		return nil, err
+	}
+
+	defer rows.Close()
+
+	var contacts []Contact
+	for rows.Next() {
+
+		var contact Contact
+		err = rows.Scan(&contact.ID, &contact.Name, &contact.RelationshipID)
+
+		if err != nil {
+			return nil, err
+		}
+
+		contacts = append(contacts, contact)
+	}
+	return contacts, nil
+}
+
+func (m *ContactModel) GetContactById(id int32) (Contact, error) {
+
+	stmt := "SELECT id, name, relationship_id FROM contacts WHERE id := ?"
+
+	result := m.DB.QueryRow(stmt, id)
+
+	var contact Contact
+	err := result.Scan(&contact.ID, &contact.Name, &contact.RelationshipID)
+
+	if err != nil {
+		return Contact{}, err
+	}
+
+	return contact, nil
+
+}
