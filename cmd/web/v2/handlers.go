623175783c92c6bd98589c9a9fe9e40c1a7a51dd
grglucastr <georgebnunes@gmail.com>
2025-05-28 21:48:40 -0300

    feat: add html templates

--- cmd/web/v2/handlers.go
+++ cmd/web/v2/handlers.go
@@ -2,13 +2,33 @@ package main
 
 import (
 	"fmt"
+	"html/template"
+	"log"
 	"net/http"
 
 	"github.com/gorilla/mux"
 )
 
 func (app *application) showPageListContacts(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintln(w, "Show page to list contacts")
+
+	files := []string{
+		"./ui/html/v2/base.html",
+		"./ui/html/v2/pages/contacts.html",
+	}
+
+	ts, err := template.ParseFiles(files...)
+	if err != nil {
+		log.Println(err.Error())
+		http.Error(w, "Internal Server Error", http.StatusInternalServerError)
+		return
+	}
+
+	err = ts.ExecuteTemplate(w, "base", nil)
+	if err != nil {
+		log.Println(err.Error())
+		http.Error(w, "Internal Server Error", http.StatusInternalServerError)
+		return
+	}
 }
 
 func (app *application) showPageContactInfos(w http.ResponseWriter, r *http.Request) {
@@ -28,15 +48,14 @@ func (app *application) showPageFormContact(w http.ResponseWriter, r *http.Reque
 	fmt.Fprintln(w, "New contact form")
 }
 
-
-func (app *application) listContacts(w http.ResponseWriter, r *http.Request){
-
-}
-
-func (app *application) getContactById(w http.ResponseWriter, r *http.Request){
+func (app *application) listContacts(w http.ResponseWriter, r *http.Request) {
 
 }
 
-func (app *application) addContact(w http.ResponseWriter, r *http.Request){
+func (app *application) getContactById(w http.ResponseWriter, r *http.Request) {
+
+}
+
+func (app *application) addContact(w http.ResponseWriter, r *http.Request) {
 
 }
